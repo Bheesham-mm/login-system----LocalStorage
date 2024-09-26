4ec748feebffdf33e9a634f083ee1567f26242d3
maheshdkushlani <maheshdkushlani@gmail.com>
2024-09-26 14:18:58 +0500

    sign_up_form_validation!

--- js/sign_up.js
+++ js/sign_up.js
@@ -10,44 +10,43 @@ var users_data = JSON.parse(localStorage.getItem('All users')) || [];
 function submitData() {
     // Validation
     if (userName.value === '') {
-        document.getElementById('paraError').innerHTML = 'user name empty'
-    }
-    else if (userEmail.value === '') {
-        document.getElementById('paraError').innerHTML = ''
-        document.getElementById('paraError1').innerHTML = 'user email empty'
-    }
-    else if (userPassword.value === '') {
-        document.getElementById('paraError').innerHTML = ''
-        document.getElementById('paraError1').innerHTML = ''
-        document.getElementById('paraError2').innerHTML = 'user password empty'
-    }
-    else {
-        // Add new user data to the array
-        users_data.push({ name: userName.value, email: userEmail.value, password: userPassword.value });
-
-        // Update localStorage with the new data
-        localStorage.setItem('All users', JSON.stringify(users_data));
-
-        // Clear the input fields after submission
-        userName.value = '';
-        userEmail.value = '';
-        userPassword.value = '';
-
-        // get storage
-        var storageCheck = JSON.parse(localStorage.getItem('All users'))
-        console.log(storageCheck)
-        // open home
-        var user = storageCheck.find(function (user) {
+        document.getElementById('paraError').innerHTML = 'user name empty';
+    } else if (userEmail.value === '') {
+        document.getElementById('paraError').innerHTML = '';
+        document.getElementById('paraError1').innerHTML = 'user email empty';
+    } else if (userPassword.value === '') {
+        document.getElementById('paraError').innerHTML = '';
+        document.getElementById('paraError1').innerHTML = '';
+        document.getElementById('paraError2').innerHTML = 'user password empty';
+    } else {
+        // Check if the email is already registered
+        var existingUser = users_data.find(function (user) {
             return user.email === userEmail.value;
         });
-        if (user) {
-            window.location.href = './login.html'
-            console.log(storageCheck)
+
+        if (existingUser) {
+            // Show alert if the email is already registered
+            alert('Your account has already been registered.');
         } else {
-            alert('your account is already registered?');
+            // Add new user data to the array
+            users_data.push({ name: userName.value, email: userEmail.value, password: userPassword.value });
+
+            // Update localStorage with the new data
+            localStorage.setItem('All users', JSON.stringify(users_data));
+
+            // Clear the input fields after submission
+            userName.value = '';
+            userEmail.value = '';
+            userPassword.value = '';
+
+            // Redirect to login page
+            window.location.href = './login.html';
         }
     }
 }
+
+
+
 //  password hide show
 function hideShow() {
     var pass = document.getElementById('password');
